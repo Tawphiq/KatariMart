2828c1f9c61164fd95528e7c54d7c41cf69291f3
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-11-03 22:16:42 +0100

    "deploying on PythonAnyWhere"

--- KatariMart/settings.py
+++ KatariMart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z@**#)!uw21omwpsh0hq@q9aee7l+bg1iz0nrft%3o21ipwiqe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -48,6 +48,7 @@ AUTH_USER_MODEL = 'UserApp.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,23 +86,14 @@ WSGI_APPLICATION = 'KatariMart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'd5feaF6ccg4g3*gD243C63-BgC*3Ccba',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '10182',
-    }
 }
+
+
 
 
 # Password validation
@@ -138,12 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
+#STATIC_URL = 'media/'
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
