f4f58c2730a3d7cdeac6cfeeb179d4d4d3bb452e
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-11-10 02:09:45 +0100

    updated the static files

--- backend/KatariMart/KatariMart/settings.py
+++ backend/KatariMart/KatariMart/settings.py
@@ -86,14 +86,14 @@ WSGI_APPLICATION = 'KatariMart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
@@ -102,7 +102,7 @@ DATABASES = {
         'HOST': 'monorail.proxy.rlwy.net',
         'PORT': '52427',
     }
-}
+}"""
 
 
 
@@ -141,10 +141,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_URL = 'media/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
-STATIC_URL = 'staticfiles_build/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build"
+STATIC_URL = 'media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
+#STATIC_URL = 'staticfiles_build/static/'
+#STATIC_ROOT = BASE_DIR / "staticfiles_build"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
@@ -152,5 +152,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
